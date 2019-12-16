43b555ca4ae8d6d494b974dc20d55c710372369e
Nikita Shevchenko <44233251+nikita-shevchenko@users.noreply.github.com>
2019-12-16 08:56:23 +0200

    try to fix commands

--- commands.py
+++ commands.py
@@ -1,16 +1,16 @@
 import click
 from flask.cli import with_appcontext
-from models import init_db
+from models import init_db, app
 from populate import populate
 
 
-@click.command(name='create_tables')
+@app.cli.command('create_tables')
 @with_appcontext
 def create_tables():
     init_db()
 
 
-@click.command(name='populate_tables')
+@app.cli.command('populate_tables')
 @with_appcontext
 def populate_tables():
     populate()
