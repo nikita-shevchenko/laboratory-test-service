e1ea34a162c792a10a7a705b53ec7c966d13d851
Nikita Shevchenko <44233251+nikita-shevchenko@users.noreply.github.com>
2019-12-16 11:18:33 +0200

    update forms and templates

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import render_template, Flask
 from forms import StudentEditForm, ResourceEditForm, LaboratoryEditForm
+from models import Test, Implementation, Label, Material, Group, Subject, Task, Resource, Student, Laboratory
 from db import db
 from commands import create_tables, populate_tables
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:3044344@127.0.0.1:5432/test'
 app.config['SECRET_KEY'] = 'Thisisasecret'
 
 db.init_app(app)
@@ -19,118 +21,175 @@ def hello_world():
     return render_template('main.html')
 
 
-# @app.route('/students', methods=['GET', 'POST'])
-# def students():
-#     form = StudentEditForm()
-#     if form.validate_on_submit():
-#         student_data = engine.execute("SELECT * FROM Student WHERE record_book='{record_book}'"
-#                                       .format(record_book=form.record_book.data))
-#         if form.delete.data:
-#             engine.execute("""
-#             DELETE FROM Student WHERE record_book='{record_book}'"""
-#                            .format(record_book=form.record_book.data))
-#         elif len(list(student_data)) == 0:
-#             engine.execute("""
-#             INSERT INTO Student (record_book, full_name, email, phone)
-#             VALUES ('{record_book}', '{full_name}', '{email}', '{phone}')"""
-#                            .format(record_book=form.record_book.data, full_name=form.full_name.data,
-#                                    email=form.email.data, phone=form.phone.data))
-#         else:
-#             engine.execute("""
-#                         UPDATE Student
-#                         SET full_name='{full_name}', email='{email}', phone='{phone}'
-#                         WHERE record_book='{record_book}'
-#                         """.format(full_name=form.full_name.data, email=form.email.data,
-#                                    phone=form.phone.data, record_book=form.record_book.data))
-#     students_list = engine.execute("SELECT * FROM Student ORDER BY record_book")
-#     return render_template('students.html', students=list(students_list), form=form)
-#
-#
-# @app.route('/resources', methods=['GET', 'POST'])
-# def resources():
-#     form = ResourceEditForm()
-#     if form.validate_on_submit():
-#         resource_data = engine.execute("SELECT * FROM resource WHERE resource_name='{resource_name}'"
-#                                       .format(resource_name=form.resource_name.data))
-#         if form.delete.data:
-#             engine.execute("""
-#             DELETE FROM resource WHERE resource_name='{resource_name}'"""
-#                            .format(resource_name=form.resource_name.data))
-#         elif len(list(resource_data)) == 0:
-#             engine.execute("""
-#              INSERT INTO resource (resource_name, label, content, rating)
-#              VALUES ('{resource_name}', '{label}', '{content}', '{rating}')"""
-#                            .format(resource_name=form.resource_name.data, label=form.label.data,
-#                                    content=form.content.data, rating=form.rating.data))
-#         else:
-#             engine.execute("""
-#                         UPDATE resource
-#                         SET label='{label}', content='{content}', rating='{rating}'
-#                         WHERE resource_name='{resource_name}'
-#                         """.format(label=form.label.data, content=form.content.data,
-#                                    rating=form.rating.data, resource_name=form.resource_name.data))
-#
-#     resource_list = engine.execute("SELECT * FROM resource")
-#     return render_template('resources.html', resources=list(resource_list), form=form)
-#
-#
-# @app.route('/laboratory', methods=['GET', 'POST'])
-# def laboratory():
-#     form = LaboratoryEditForm()
-#     if form.validate_on_submit():
-#         laboratory_data = engine.execute("SELECT * FROM laboratory WHERE number='{number}' AND variant='{variant}'"
-#                                        .format(number=form.number.data, variant=form.variant.data))
-#         if form.delete.data:
-#             engine.execute("""
-#             DELETE FROM laboratory WHERE number='{number}' AND variant='{variant}'"""
-#                            .format(number=form.number.data, variant=form.variant.data))
-#         elif len(list(laboratory_data)) == 0:
-#             engine.execute("""
-#              INSERT INTO laboratory (number, variant, task)
-#              VALUES ('{number}', '{variant}', '{task}')"""
-#                            .format(number=form.number.data, variant=form.variant.data, task=form.task.data))
-#         else:
-#             engine.execute("""
-#                         UPDATE laboratory
-#                         SET task='{task}'
-#                         WHERE number='{number}' AND variant='{variant}'
-#                         """.format(number=form.number.data, variant=form.variant.data, task=form.task.data))
-#     laboratory_list = engine.execute("SELECT * FROM laboratory")
-#     return render_template('laboratory.html', laboratory=list(laboratory_list), form=form)
-#
-#
-# @app.route('/materials')
-# def materials():
-#     headlines = ['material_theme', 'subject', 'number', 'variant', 'label', 'content']
-#     materials_list = engine.execute("SELECT * FROM material")
-#     return render_template('table.html', entity='Material', data=list(materials_list), headlines=headlines)
-#
-#
-# @app.route('/clusters')
-# def clusters():
-#     headlines = ['label', 'name']
-#     cluster_list = engine.execute("SELECT * FROM cluster")
-#     return render_template('table.html', entity='Cluster', data=list(cluster_list), headlines=headlines)
-#
-#
-# @app.route('/works')
-# def works():
-#     headlines = ['attempt', 'record_book', 'number', 'variant', 'mark', 'test_result', 'content', 'operator_sequence',
-#                  'plagiary']
-#     work_list = engine.execute("SELECT * FROM work")
-#     return render_template('table.html', entity='Work', data=list(work_list), headlines=headlines)
-#
-#
-# @app.route('/tests')
-# def tests():
-#     headlines = ['test_name', 'number', 'variant', 'input_data', 'expected_result', 'on_fail_message']
-#     test_list = engine.execute("SELECT * FROM test")
-#     return render_template('table.html', entity='Test', data=list(test_list), headlines=headlines)
-#
-#
-# @app.teardown_appcontext
-# def shutdown_session(exception=None):
-#     db_session.remove()
+@app.route('/students', methods=['GET', 'POST'])
+def students():
+    form = StudentEditForm()
+    if form.validate_on_submit():
+        student = Student.query.filter_by(record_book=form.record_book.data).first()
+        if form.delete.data:
+            db.session.delete(student)
+            db.session.commit()
+        elif student is None:
+            new_student = Student(form.record_book.data, form.group_year.data, form.group_name.data,
+                                  form.student_name.data, form.student_email.data, form.student_phone.data)
+            db.session.add(new_student)
+            db.session.commit()
+        else:
+            student.group_year = form.group_year.data
+            student.student_name = form.student_name.data
+            student.group_name = form.group_name.data
+            student.student_email = form.student_email.data
+            student.student_phone = form.student_phone.data
+            db.session.commit()
+    headlines = ['record_book', 'group_year', 'group_name', 'student_name', 'student_email', 'student_phone',
+                 'delete?', 'action']
+    results = Student.query.all()
+    student_list = []
+    for i in range(len(results)):
+        student_list.append(results[i])
+    return render_template('students.html', students=student_list, form=form, headlines=headlines)
+
+
+@app.route('/resources', methods=['GET', 'POST'])
+def resources():
+    form = ResourceEditForm()
+    if form.validate_on_submit():
+        resource = Resource.query.filter_by(resource_name=form.resource_name.data).first()
+        if form.delete.data:
+            db.session.delete(resource)
+            db.session.commit()
+        elif resource is None:
+            new_resource = Resource(form.resource_name.data, form.resource_source.data,
+                                    form.label_number.data, form.resource_content.data, form.rating.data)
+            db.session.add(new_resource)
+            db.session.commit()
+        else:
+            resource.resource_source = form.resource_source.data
+            resource.label_number = form.label_number.data
+            resource.resource_content = form.resource_content.data
+            resource.rating = form.rating.data
+            db.session.commit()
+
+    headlines = ['resource_name', 'resource_source', 'label_number', 'resource_content', 'rating', 'delete?', 'action']
+    results = Resource.query.all()
+    resource_list = []
+    for i in range(len(results)):
+        resource_list.append(results[i])
+    return render_template('resources.html', resources=resource_list, form=form, headlines=headlines)
+
+
+@app.route('/laboratory', methods=['GET', 'POST'])
+def laboratory():
+    form = LaboratoryEditForm()
+    if form.validate_on_submit():
+        lab = Laboratory.query.filter_by(laboratory_theme=form.laboratory_theme.data).first()
+        if form.delete.data:
+            db.session.delete(lab)
+            db.session.commit()
+        elif lab is None:
+            new_lab = Laboratory(form.laboratory_theme.data, form.subject_name.data,
+                                 form.laboratory_goal.data, form.laboratory_number.data)
+            db.session.add(new_lab)
+            db.session.commit()
+        else:
+            lab.subject_name = form.subject_name.data
+            lab.laboratory_goal = form.laboratory_goal.data
+            lab.laboratory_number = form.laboratory_number.data
+            db.session.commit()
+    headlines = ['laboratory_theme', 'subject_name', 'laboratory_goal', 'laboratory_number', 'delete?', 'action']
+    results = Laboratory.query.all()
+    laboratory_list = []
+    for i in range(len(results)):
+        laboratory_list.append(results[i])
+    return render_template('laboratory.html', laboratory=laboratory_list, form=form, headlines=headlines)
+
+
+@app.route('/tasks')
+def tasks():
+    headlines = ['variant', 'laboratory_theme', 'laboratory_task']
+    results = Task.query.all()
+    task_list = []
+    for i in range(len(results)):
+        task_list.append([results[i].variant, results[i].laboratory_theme, results[i].laboratory_task])
+    return render_template('table.html', entity='Task', data=task_list, headlines=headlines)
+
+
+@app.route('/groups')
+def groups():
+    headlines = ['group_name', 'group_year']
+    results = Group.query.all()
+    group_list = []
+    for i in range(len(results)):
+        group_list.append([results[i].group_name,
+                           results[i].group_year])
+    return render_template('table.html', entity='Group', data=group_list, headlines=headlines)
+
+
+@app.route('/subjects')
+def subjects():
+    headlines = ['subject_name']
+    results = Subject.query.all()
+    subject_list = []
+    for i in range(len(results)):
+        subject_list.append([results[i].subject_name])
+    return render_template('table.html', entity='Subject', data=subject_list, headlines=headlines)
+
+
+@app.route('/materials')
+def materials():
+    headlines = ['material_name', 'material_author', 'label_number', 'material_content']
+    results = Material.query.all()
+    material_list = []
+    for i in range(len(results)):
+        material_list.append([results[i].material_name,
+                              results[i].material_author,
+                              results[i].label_number,
+                              results[i].material_content])
+    return render_template('table.html', entity='Material', data=material_list, headlines=headlines)
+
+
+@app.route('/labels')
+def labels():
+    headlines = ['label_number', 'label_name']
+    results = Label.query.all()
+    label_list = []
+    for i in range(len(results)):
+        label_list.append([results[i].label_number,
+                           results[i].label_name])
+    return render_template('table.html', entity='Cluster', data=label_list, headlines=headlines)
+
+
+@app.route('/implementations')
+def implementations():
+    headlines = ['attempt', 'record_book', 'group_year', 'laboratory_theme', 'mark',
+                 'implementation_content', 'test_output', 'operator_sequence', 'plagiary']
+    results = Implementation.query.all()
+    implementation_list = []
+    for i in range(len(results)):
+        implementation_list.append([results[i].attempt,
+                                    results[i].record_book,
+                                    results[i].group_year,
+                                    results[i].laboratory_theme,
+                                    results[i].mark,
+                                    results[i].implementation_content,
+                                    results[i].test_output,
+                                    results[i].operator_sequence,
+                                    results[i].plagiary,
+                                    ])
+    return render_template('table.html', entity='Work', data=implementation_list, headlines=headlines)
+
+
+@app.route('/tests')
+def tests():
+    headlines = ['test_name', 'input_data', 'expected_result', 'output_data']
+    results = Test.query.all()
+    test_list = []
+    for i in range(len(results)):
+        test_list.append([results[i].test_name,
+                          results[i].input_data,
+                          results[i].expected_result,
+                          results[i].output_data])
+    return render_template('table.html', entity='Test', data=test_list, headlines=headlines)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -5,23 +5,26 @@ from wtforms.validators import Email, regexp
 
 class StudentEditForm(FlaskForm):
     record_book = StringField('record_book')
-    full_name = StringField('student_name')
+    group_year = IntegerField('group_year')
+    student_name = StringField('student_name')
     group_name = StringField('group_name')
-    email = StringField('student_email', validators=[Email()])
-    phone = StringField('student_phone')
+    student_email = StringField('student_email', validators=[Email()])
+    student_phone = StringField('student_phone')
     delete = BooleanField('delete', default=False)
 
 
 class ResourceEditForm(FlaskForm):
     resource_name = StringField('resource_name')
-    label = IntegerField('label')
-    content = StringField('content')
+    resource_source = StringField('resource_source')
+    label_number = IntegerField('label_number')
+    resource_content = StringField('resource_content')
     rating = IntegerField('rating')
     delete = BooleanField('delete', default=False)
 
 
 class LaboratoryEditForm(FlaskForm):
-    number = IntegerField('number')
-    variant = IntegerField('variant')
-    task = StringField('task')
+    laboratory_theme = StringField('laboratory_theme')
+    subject_name = StringField('subject_name')
+    laboratory_goal = StringField('laboratory_goal')
+    laboratory_number = IntegerField('laboratory_number')
     delete = BooleanField('delete', default=False)
