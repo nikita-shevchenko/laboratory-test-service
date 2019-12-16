5f02632ba326f29d38bb5d019f59df4adec1b80a
Nikita Shevchenko <44233251+nikita-shevchenko@users.noreply.github.com>
2019-12-16 02:19:18 +0200

    fix db.py hell

--- db.py
+++ db.py
@@ -2,12 +2,6 @@ from sqlalchemy import create_engine
 import os
 
 
-POSTGRES_URL = os.environ.get('DATABASE_URL')
-POSTGRES_USER = "postgres"
-POSTGRES_PW = "3044344"
-POSTGRES_DB = "courses"
-
-DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
-    user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
+DB_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(DB_URL)
