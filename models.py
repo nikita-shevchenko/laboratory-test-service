8f75c76df95d9445d1d114557ed7d40a8210be57
nikita-shevchenko <nish@opinov8.com>
2019-12-23 13:32:53 +0200

    library model added

--- models.py
+++ models.py
@@ -2,9 +2,12 @@ from db import db
 class Library(db.Model):
     library_name = db.Column(db.String(500), primary_key=True)
     library_address = db.Column(db.String(500))
-    record_book = db.Column(db.String(6), db.ForeignKey('student.record_book'), primary_key=True)
+    record_book = db.Column(db.String(6), primary_key=True)
+    group_year = db.Column(db.Integer, primary_key=True)
     library_city = db.Column(db.String(500), primary_key=True)
     library_country = db.Column(db.String(500), primary_key=True)
+    __table_args__ = (db.ForeignKeyConstraint(('record_book', 'group_year'),
+                                              ('student.record_book', 'student.group_year')), {})
 
     def __init__(self, library_name, library_address, record_book, library_city, library_country):
         self.library_name = library_name
